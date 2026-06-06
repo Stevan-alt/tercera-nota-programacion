9ffd3df04439de2146c856526b8e39f9a147d12e
Stevan-alt <stevan66080@gmail.com>
2026-06-06 07:35:38 -0500

    Add Ejercicio5 to generate and sort random numbers

---
+++ src/unidad 2/Ejercicio4.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+
+        int[] vector = new int[10];
+        Random random = new Random();
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = random.nextInt(100);
+        }
+
+        Arrays.sort(vector);
+
+        System.out.println("Vector ordenado:");
+
+        for (int numero : vector) {
+            System.out.println(numero);
+        }
+    }
+}
